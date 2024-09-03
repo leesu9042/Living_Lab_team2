062227db8b87cf06cbae6a022b313099550f5603
leesu9042 <128389341+leesu9042@users.noreply.github.com>
2024-09-03 19:34:18 +0900

    Delete livinglab directory

--- livinglab/src/main/java/com/example/livinglab/repository/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.livinglab.repository;
-
-import com.example.livinglab.entity.Schedule;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
-}
